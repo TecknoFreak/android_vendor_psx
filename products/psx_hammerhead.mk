2ac5701de9c69d5b57c6813e77c87dc0d6612274
chad <chadclough@gmail.com>
2013-11-05 09:08:23 -0600

    One more

---
+++ products/psx_hammerhead.mk
@@ -0,0 +1,15 @@
+# Inherit AOSP device configuration for mako.
+$(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/psx/products/common.mk)
+
+# Setup device specific product configuration.
+PRODUCT_NAME := psx_hammerhead
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := hammerhead
+PRODUCT_MODEL := Nexus 5
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT="fingerprint: google/hammerhead/hammerhead:4.4/KRT16M/893803:user/release-keys" PRIVATE_BUILD_DESC="hammerhead-user 4.4 KRT16M 893803 release-keys"
+
