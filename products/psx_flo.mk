505dae3a5a9f50683a20ce7f4042fb6899002ed6
chad <chadclough@gmail.com>
2013-11-09 17:08:47 -0600

    One more Flo file

---
+++ products/psx_flo.mk
@@ -0,0 +1,15 @@
+# Inherit AOSP device configuration for flo.
+$(call inherit-product, device/asus/flo/full_flo.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/psx/products/common.mk)
+
+# Setup device specific product configuration.
+PRODUCT_NAME := psx_flo
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := flo
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := Asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razor BUILD_FINGERPRINT="google/razor/flo:4.4/KRT16M/737497:user/release-keys" PRIVATE_BUILD_DESC="razor-user 4.4 KRT16M 737497 release-keys"
+
