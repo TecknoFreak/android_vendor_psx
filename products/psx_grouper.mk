2c15f5c870ddf56d91e75d4bebca13cef0ad86fd
Martin Rodemann <martin@rodemann.org>
2013-12-19 11:18:35 +0100

    KOT49H fingerprints for flo,manta and grouper

--- products/psx_grouper.mk
+++ products/psx_grouper.mk
@@ -11,4 +11,4 @@ PRODUCT_DEVICE := grouper
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := Asus
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:4.4/KRT16S/737497:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.4 KRT16S 737497 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT=google/nakasi/grouper:4.4.2/KOT49H/937116:user/release-keys PRIVATE_BUILD_DESC="nakasi-user 4.4.2 KOT49H 937116 release-keys"
--- products/psx_manta.mk
+++ products/psx_manta.mk
@@ -11,5 +11,6 @@ PRODUCT_DEVICE := manta
 PRODUCT_MODEL := Nexus 10
 PRODUCT_MANUFACTURER := Samsung
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mantaray BUILD_FINGERPRINT=google/mantaray/manta:4.4/KRT16S/737497:user/release-keys PRIVATE_BUILD_DESC="mantaray-user 4.4 KRT16S 737497 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mantaray BUILD_FINGERPRINT="google/mantaray/manta:4.4.2/KOT49H/937116:user/release-keys" PRIVATE_BUILD_DESC="mantaray-user 4.4.2 KOT49H 937116 release-keys"
+
 
